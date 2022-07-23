3b1eb67ed083b8cb866aba37bb9dde6cde19182f
Ruchigevariya <gevariyaruchita@gmail.com>
2022-07-23 19:07:15 +0530

    change

--- src/redux/action/Counter.action.js
+++ src/redux/action/Counter.action.js
@@ -1,4 +1,4 @@
-import * as ActionTypes from '../ActionTypes';
+import * as ActionTypes from '../../redux/ActionTypes';
 
 export const increment = () => (dispatch) => {
     dispatch({type : ActionTypes.INCREAMENT_COUNTER})
