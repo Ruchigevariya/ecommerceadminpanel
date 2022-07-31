1998b6129e14a07d9aeed6f4e23a38a326e43cff
Ruchigevariya <gevariyaruchita@gmail.com>
2022-07-31 22:41:01 +0530

    changes

--- src/redux/Action/Product.action.js
+++ src/redux/Action/Product.action.js
@@ -4,7 +4,7 @@ export const getProduct = () => (dispatch) => {
     try{
         fetch('http://localhost:3004/product')
         .then((response) => response.json())
-        .then((data) => dispatch({type: ActionTypes.GET_PRODUCTDATA, payload: data}));
+        .then((data) => dispatch({type: ActionTypes.GET_PRODUCTDATA, payload: data}))
     }catch (error){  
         console.log(error.message);
     }
